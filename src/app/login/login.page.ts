979e74c6de584c4d0b4d8adb6204582d88d405c0
Miguel Astorga <xmiguelastorgax@gmail.com>
2024-08-31 15:45:58 -0400

    Descripción de los cambios que realizaste en el login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SessionManager } from 'src/managers/SessionManager';
-import { User } from "../models/user.models";
-import { LoadingController, NavController, ToastController } from '@ionic/angular';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-
 
 @Component({
   selector: 'app-login',
@@ -12,64 +8,30 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  user = {} as User;
 
-  constructor(
-    private router: Router,
-    private toastCtrl: ToastController,
-    private loadingCtrl: LoadingController,
-    private afAuth: AngularFireAuth,
-    private navCtrl: NavController) { }
+  constructor(private router: Router, private sessionManager: SessionManager) { }
 
-    
+    user: string = '';
+    password: string = '';
 
   ngOnInit() {
   }
-  async onLoginButtonPressed(user: User){
-    if(this.formValidation()){
-      let loader = await this.loadingCtrl.create({message:"Espere por favor"})
-      await loader.present();
 
-      try{
-        await this.afAuth.signInWithEmailAndPassword(user.email,user.password).then(data =>{
-          console.log(data);
-
-          this.navCtrl.navigateRoot("home")
-        })
-
-      }catch (error:any){
-        error.message = "Usuario no registrado";
-        let errormessage = error.message || error.getLocalizedMessage();
-
-        this.showToast(errormessage)
-      }
-      await loader.dismiss();
+  onLoginButtonPressed() {
+    if(this.sessionManager.performLogin(this.user, this.password)) {
+      this.router.navigate(['/home'])
+    } else {
+      this.user=''
+      this.password=''
+      alert('Las credenciales ingresadas son inválidas.')
     }
   }
-  formValidation(){
-    if(!this.user.email){
-      this.showToast("Ingrese un email");
-      return false;
-    }
-    if(!this.user.password){
-      this.showToast("Ingrese una contraseña");
-      return false;
-  }
-  return true;
 
-  
-  }
-  showToast(message: string){
-    this.toastCtrl.create({
-      message: message,
-      duration: 4000
-    }).then(toastData => toastData.present());
-  }
+  onRegisterButtonPressed() {
+    this.router.navigate(['/register'])
   }
 
-
-
-
+}
 
  
 
--- src/app/splash/splash.page.ts
+++ src/app/splash/splash.page.ts
@@ -7,12 +7,28 @@ import { Router } from '@angular/router'
   styleUrls: ['./splash.page.scss'],
 })
 export class SplashPage implements OnInit {
-
+  progressValue = 0;
   constructor(private router: Router) { }
 
   ngOnInit() {
+    this.startProgressBar();
     setTimeout(() => {
-      this.router.navigate(['/login']);  
-    }, 2000)
+      this.router.navigate(['/login']);
+    }, 20000); // Navegar a /login después de 20 segundos
+  }
+  startProgressBar() {
+    const duration = 20000; // Duración del splash en milisegundos (20 segundos)
+    const interval = 100;   // Intervalo de actualización (100 ms)
+    const increment = interval / duration;
+
+    let currentProgress = 0;
+    const progressInterval = setInterval(() => {
+      currentProgress += increment;
+      this.progressValue = Math.min(currentProgress, 1); // Asegúrate de que el valor no supere 1
+
+      if (currentProgress >= 1) {
+        clearInterval(progressInterval);
+      }
+    }, interval);
   }
 }
