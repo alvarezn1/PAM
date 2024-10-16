4f50502c89b6ab2075a2b2097ce6d56ff791098a
unknown <xmiguelastorgax@gmail.com>
2024-10-15 23:01:19 -0300

    mejorado el diseño

--- src/app/splash/splash.page.ts
+++ src/app/splash/splash.page.ts
@@ -16,12 +16,17 @@ export class SplashPage implements OnInit {
     this.startProgressBar();
     setTimeout(() => {
       this.checkSession();
-    }, 2000); // Navegar a la verificación de sesión después de 20 segundos
+    }, 2000); // Ajuste del tiempo para que la barra termine primero
+  }
+
+  ionViewWillEnter() {
+    this.progressValue = 0;  // Reiniciar la barra de progreso cada vez que se entra a la vista
+    this.startProgressBar();
   }
 
   startProgressBar() {
-    const duration = 2000; // Duración del splash en milisegundos (20 segundos)
-    const interval = 100;    // Intervalo de actualización (100 ms)
+    const duration = 2000; // Duración del splash en milisegundos (2 segundos)
+    const interval = 100;  // Intervalo de actualización (100 ms)
     const increment = interval / duration;
 
     let currentProgress = 0;
